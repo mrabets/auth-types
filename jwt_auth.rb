fec1d8efe1461b8afa65edebee070d0b3a58f563
Karim Marabet <karim.marabet@productmadness.com>
2022-12-13 08:19:24 +0100

    Add JWT Auth

--- jwt_auth.rb
+++ jwt_auth.rb
@@ -3,11 +3,31 @@ require 'pry'
 require "base64"
 require 'json'
 require 'securerandom'
+require 'jwt'
 
-AVAILABLE_TOKENS = []
+def admin_login_password
+  "admin:admin"
+end
+
+def valid_token?(jwt_token)
+  decoded_token = JWT.decode current_bearer_token, nil, false
+  # Array
+  # [
+  #   {"data"=>"admin:admin"}, # payload
+  #   {"alg"=>"none"} # header
+  # ]
+  decoded_token[0]["data"] == admin_login_password
+rescue
+  false
+end
 
 def generate_token
-  SecureRandom.hex.tap { |token| AVAILABLE_TOKENS.push(token) }
+  payload = { data: current_auth_data }
+  JWT.encode payload, nil, 'none'
+end
+
+def http_auth_data
+  request.env.fetch("HTTP_AUTHORIZATION")
 end
 
 def current_bearer_token
@@ -15,8 +35,13 @@ def current_bearer_token
   http_auth_data[7..]
 end
 
-def http_auth_data
-  request.env["HTTP_AUTHORIZATION"]
+def current_auth_data
+  # delete 'Basic '
+  Base64.decode64(http_auth_data[6..])
+end
+
+def current_jwt_cookie_token
+  request.cookies["jwt_token"]
 end
 
 before do
@@ -24,9 +49,10 @@ before do
 end
 
 get '/' do
-  if http_auth_data.nil?
-    generate_token
-  elsif AVAILABLE_TOKENS.include?(current_bearer_token)
+  response.set_cookie("jwt_token", generate_token) unless current_jwt_cookie_token
+
+  # Both: cookie and bearer 
+  if valid_token?(current_jwt_cookie_token) && valid_token?(current_bearer_token)
     'Success! :)'
   else
     'Failed :('
